928e6c5fb622cdab0df3d7079d9db28651e4c19a
folathecoder <officialakinloyefolarin@gmail.com>
2021-10-23 09:26:56 +0100

    Checkout Modal ==== Completed && Responsive

--- components/checkout/checkoutModal/CheckoutModalStyles.ts
+++ components/checkout/checkoutModal/CheckoutModalStyles.ts
@@ -7,14 +7,18 @@ export const ModalContainer = styled.section`
   top: 50%;
   transform: translate(-50%, -50%);
   width: 33.75rem;
-  height: 36.3125rem;
+  min-height: 36.3125rem;
   border-radius: var(--border-radius);
   z-index: 3000;
   padding: 2.9375rem;
+
+  @media screen and (max-width: 585px) {
+    width: 20.4375rem;
+    padding: 1rem;
+    min-height: 0rem;
+  }
 `;
-export const ModalWrap = styled.div`
-  /* background-color: yellow; */
-`;
+export const ModalWrap = styled.div``;
 
 export const ModalIconWrap = styled.div`
   position: relative;
@@ -22,6 +26,11 @@ export const ModalIconWrap = styled.div`
   width: 4rem;
   display: flex;
   justify-content: flex-start;
+
+  @media screen and (max-width: 585px) {
+    height: 3rem;
+    width: 3rem;
+  }
 
   img {
     object-fit: contain;
@@ -31,12 +40,18 @@ export const ModalHeader = styled.h2`
   text-transform: uppercase;
   font-size: 2rem;
   line-height: 2.25rem;
-  /* max-width: 16rem; */
   margin: 2rem 0rem;
+
+  @media screen and (max-width: 585px) {
+    font-size: 1.6rem;
+    line-height: 2rem;
+    margin: 1rem 0rem;
+  }
 `;
 export const ModalMessage = styled.p``;
+
 export const ModalSummary = styled.div`
-  height: 8.75rem;
+  min-height: 8.75rem;
   width: 100%;
   border-radius: var(--border-radius);
   margin: 2rem 0rem 3rem 0rem;
@@ -44,6 +59,12 @@ export const ModalSummary = styled.div`
 
   & > * {
     flex: 1;
+  }
+
+  @media screen and (max-width: 585px) {
+    flex-direction: column;
+    min-height: 8.75rem;
+    margin: 1rem 0rem 1rem 0rem;
   }
 `;
 
@@ -55,17 +76,26 @@ export const ModalSummaryDetails = styled.div`
   padding: 0rem 1rem;
   flex: 1 3rem;
 
+  @media screen and (max-width: 585px) {
+    border-radius: 0.5rem 0.5rem 0rem 0rem;
+  }
+
   & > * {
     flex: 1;
   }
 `;
-export const CartItemWrap = styled.div``;
+
+export const CartItemWrap = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
 export const CartItemMore = styled.div`
   flex: 1 3.3rem;
   display: flex;
   align-items: center;
   justify-content: center;
+  border-top: 0.1rem solid var(--color-sec-600);
 
   p {
     font-size: 0.8rem;
@@ -80,6 +110,11 @@ export const ModalSummaryTotal = styled.div`
   border-radius: 0rem 0.5rem 0.5rem 0rem;
   display: flex;
   align-items: center;
+
+  @media screen and (max-width: 585px) {
+    min-height: 6.75rem;
+    border-radius: 0rem 0rem 0.5rem 0.5rem;
+  }
 `;
 
 export const ModalTotal = styled.div`
