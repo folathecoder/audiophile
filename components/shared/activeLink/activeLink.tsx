7d079e1a3f31b030064392b7d113fe27e610e59a
folathecoder <officialakinloyefolarin@gmail.com>
2021-10-24 17:01:05 +0100

    Active Link Component ==== Improved Commenting

--- components/shared/activeLink/activeLink.tsx
+++ components/shared/activeLink/activeLink.tsx
@@ -1,18 +1,22 @@
 import { withRouter } from "next/router";
 
 const ActiveLink = ({ router, href, children }): JSX.Element => {
+  //TODO: One-Time function that triggers prefetching of pages
   (function prefetchPages() {
     if (typeof window !== "undefined") {
       router.prefetch(router.pathname);
     }
   })();
 
+  //TODO: Prevent page from reloading and correctly redirect to the desired page
   const handleClick = (e) => {
     e.preventDefault();
     router.push(href);
   };
 
+  //TODO: Check if the current path is the same as the router path
   const isCurrentPath = router.pathname === href || router.asPath === href;
+  
   return (
     <>
       <a
